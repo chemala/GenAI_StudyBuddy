14f65411caac273c4c4e11cf9556ef6219e10c68
Aykut Tayfun Güneş <lidertakimi@gmail.com>
2026-01-18 20:50:55 +0100

    Deleted Token/Keys

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TAVILY_API_KEY = "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" #1000 Free Credits / Month
-HF_TOKEN = "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"
+TAVILY_API_KEY = ""
+HF_TOKEN = ""
 
 MODEL_ID = "Qwen/Qwen2.5-7B-Instruct"
 
