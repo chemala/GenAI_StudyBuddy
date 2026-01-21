cfc5a2107af1bb197a2282ed5837e85295f847ad
makieew <marta.raseta01@gmail.com>
2026-01-21 22:18:58 +0100

    retrieval scoring

--- embeddings/embedder.py
+++ embeddings/embedder.py
@@ -4,4 +4,4 @@ embedder = SentenceTransformer("all-mpnet-base-v2")
 EMB_DIM = embedder.get_sentence_embedding_dimension()
 
 def embed(texts):
-    return embedder.encode(texts, convert_to_numpy=True).astype("float32")
+    return embedder.encode(texts, normalize_embeddings=True, convert_to_numpy=True).astype("float32")
