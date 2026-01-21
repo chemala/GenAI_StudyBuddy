423946774abdd6610389e8b80a7a17124d457d28
Aykut Tayfun Güneş <lidertakimi@gmail.com>
2026-01-21 22:07:47 +0100

    Config update

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-TAVILY_API_KEY = "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"
-HF_TOKEN = "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"
+TAVILY_API_KEY = ""
+HF_TOKEN = ""
 
 MODEL_ID = "Qwen/Qwen2.5-7B-Instruct"
 
 CHUNK_SIZE = 1200
 CHUNK_OVERLAP = 240
-TOP_K = 12
+TOP_K = 5
